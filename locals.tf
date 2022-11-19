5067b0323e51905f30e99df60345e1b6358d7c0c
sambo2021 <mohamedsambomahmoud@gmail.com>
2022-11-19 16:01:27 +0200

    adding more than one private instance and see how to ssh to

--- locals.tf
+++ locals.tf
@@ -7,5 +7,15 @@ locals {
     owners = local.owners
     environment = local.environment
   }
+  public_ec2_tags ={
+    owners = local.owners
+    environment = local.environment
+    type = "public"
+  }
+  private_ec2_tags ={
+    owners = local.owners
+    environment = local.environment
+    type = "private"
+  }
   eks_cluster_name = "${local.name}-${var.cluster_name}"  
 } 
