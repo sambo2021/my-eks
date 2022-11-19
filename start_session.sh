b348d234deaa9012b74e6d2c4a3dcbc157f43536
sambo2021 <mohamedsambomahmoud@gmail.com>
2022-11-19 23:15:31 +0200

    fix syntax error of start session script

--- start_session.sh
+++ start_session.sh
@@ -6,4 +6,4 @@ export PrivateIP=`aws ec2 describe-instances --region "us-east-2" --query "Reser
 export PublicIP=`aws ec2 describe-instances --region "us-east-2" --query "Reservations[*].Instances[*].{PublicIP:NetworkInterfaces[*].Association.PublicIp}" --filter "Name=tag:Name,Values=$INSTANCE" | jq -r .[0][0]."PublicIP"[0]`
 export BastionPublicIP=`aws ec2 describe-instances --region "us-east-2" --query "Reservations[*].Instances[*].{PublicIP:NetworkInterfaces[*].Association.PublicIp}" --filter "Name=tag:Name,Values=DXL-Dev-BastionHost" | jq -r .[0][0]."PublicIP"[0]`
 echo " >>>>>>> connecting to $INSTANCE"
-ssh -o ProxyCommand='ssh -W %h:%p -q -oStrictHostKeyChecking=no -i $PWD/private-key/eks-terraform-key.pem ec2-user@$BastionPublicIP' -oStrictHostKeyChecking=no -i $PWD/private-key/eks-terraform-key.pem ec2-user@$PrivateIP
+ssh -o ProxyCommand="ssh -W %h:%p -q -oStrictHostKeyChecking=no -i $PWD/private-key/eks-terraform-key.pem ec2-user@$BastionPublicIP" -oStrictHostKeyChecking=no -i $PWD/private-key/eks-terraform-key.pem ec2-user@$PrivateIP
