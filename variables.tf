18a2c787ce15f35c10d896c9e14b419bebea74e2
sambo2021 <mohamedsambomahmoud@gmail.com>
2022-11-24 01:06:01 +0200

    creating eks clusret as first trial all public

--- variables.tf
+++ variables.tf
@@ -112,12 +112,12 @@ variable "cluster_name" {
 variable "cluster_service_ipv4_cidr" {
   description = "service ipv4 cidr for the kubernetes cluster"
   type        = string
-  default     = null
+  default     = "172.20.0.0/16"
 }
 variable "cluster_version" {
   description = "Kubernetes minor version to use for the EKS cluster (for example 1.21)"
   type = string
-  default     = null
+  default     = "1.22"
 }
 variable "cluster_endpoint_private_access" {
   description = "Indicates whether or not the Amazon EKS private API server endpoint is enabled."
